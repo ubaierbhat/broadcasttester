1c039f8595380a0428d64ed07310af81369a1d1c
Ubaier Bhat <ubaier.bhat@omniva.ee>
2025-08-30 08:24:46 +0300

    update target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.ubaierbhat.broadcasttester"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ubaierbhat.broadcasttester"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
